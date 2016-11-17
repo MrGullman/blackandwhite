09d50333803c15612342843ed7015dc495ee07a7
MrGullman <jesper.gullman@outlook.com>
2016-11-17 01:11:27 +0100

    Tweeking
    Ändrade menyn la till fonter, började arbeta med main content samt
    photgraper

---
+++ js/photographers.js
@@ -0,0 +1,19 @@
+var photoG = [
+  
+  {
+    name: "Jesper Gullman",
+    country: "Sweden",
+    profileImg: "../img/me.jpg",
+    images: [
+      "../img/boat01.jpg",
+      "../img/boat02.jpg", 
+      "../img/boat03.jpg",
+      "../img/sq_boat01.jpg",
+      "../img/sq_boat02.jpg", 
+      "../img/sq_boat03.jpg",
+    ]
+  }
+  
+]
+
+console.log(photoG[0]);
--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 /* Basic scripts for the page */
+$(document).ready(function(){
+  
+  $("#test").next().append("<p>Detta kanske fungerar</p>");
+  $("#test").html(photoG[0].country);
+  
+  
+  
+  
+});
